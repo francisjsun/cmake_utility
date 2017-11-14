f647a136b62061603b8ef64a0f92cb7c25e5c743
Francis Sun <francisjsun@outlook.com>
2017-11-14 18:52:28 +0800

    changing install dir

--- Findlibtheron.cmake
+++ Findlibtheron.cmake
@@ -3,10 +3,10 @@ if(UNIX)
   set(DEFAULT_INSTALL_DIR "/usr/local/")
 endif()
 
-find_library(theron_lib NAMES theron PATH ${DEFAULT_INSTALL_DIR} PATH_SUFFIXES "Theron/Lib")
-find_library(theron_lib_d NAMES therond PATH ${DEFAULT_INSTALL_DIR} PATH_SUFFIXES "Theron/Lib")
+find_library(theron_lib NAMES theron PATH ${DEFAULT_INSTALL_DIR} PATH_SUFFIXES "lib/Theron")
+find_library(theron_lib_d NAMES therond PATH ${DEFAULT_INSTALL_DIR} PATH_SUFFIXES "lib/Theron")
 
-set(theron_include_dir ${DEFAULT_INSTALL_DIR}Theron/Include)
+set(theron_include_dir ${DEFAULT_INSTALL_DIR}/include/Theron)
 
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(libtheron DEFAULT_MSG theron_lib theron_include_dir)
