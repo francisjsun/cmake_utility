b9bb1b0d831370d70d56afd271d58e3058dac8ef
Francis Sun <francisjsun@outlook.com>
2017-12-13 19:01:30 +0800

    fixed issue happened when using gb_add_class in subdirectory CMakeList.txt

--- gbCommon.cmake
+++ gbCommon.cmake
@@ -25,7 +25,7 @@ include_directories(SYSTEM ${GB_DEFAULT_INSTALL_DIR})
 # search class_name.[h|cpp] in class_dir and add to buffer
 macro(gb_add_class class_name class_dir buffer)
   
-  set(src_file ${CMAKE_SOURCE_DIR}/${class_dir}/${class_name}.cpp)
+  set(src_file ${CMAKE_CURRENT_SOURCE_DIR}/${class_dir}/${class_name}.cpp)
   if(EXISTS ${src_file})
     set(${buffer} ${${buffer}} ${src_file})
     message(STATUS "src_file@ " ${src_file} " added")
@@ -33,7 +33,7 @@ macro(gb_add_class class_name class_dir buffer)
     message("src_file@ " ${src_file} " not found")
   endif()
 
-  set(header_file ${CMAKE_SOURCE_DIR}/${class_dir}/${class_name}.h)
+  set(header_file ${CMAKE_CURRENT_SOURCE_DIR}/${class_dir}/${class_name}.h)
   if(EXISTS ${header_file})
     set(${buffer} ${${buffer}} ${header_file})
     message(STATUS "header_file@ " ${header_file} " added")
