6dfd993c990b50e7f24e3d037d1bf3c93ead6b47
Francis Sun <francisjsun@outlook.com>
2019-01-02 14:39:02 +0800

    replaced gb with fs

--- fsCommon.cmake
+++ fsCommon.cmake
@@ -6,7 +6,7 @@ endif()
 
 if(NOT CMAKE_BUILD_TYPE)
   set(CMAKE_BUILD_TYPE "Debug")
-  set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -DGB_DEBUG=1")
+  set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -DFS_DEBUG=1")
 endif()
 
 if(WIN32)
@@ -16,16 +16,16 @@ set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wall")
 endif()
 
 
-set(GB_DEFAULT_INSTALL_DIR)
+set(FS_DEFAULT_INSTALL_DIR)
 if(WIN32)
-  set(GB_DEFAULT_INSTALL_DIR "C:/Program\ Files\ (x86)")
+  set(FS_DEFAULT_INSTALL_DIR "C:/Program\ Files\ (x86)")
 else()
-  set(GB_DEFAULT_INSTALL_DIR "/usr/local")
+  set(FS_DEFAULT_INSTALL_DIR "/usr/local")
 endif()
 
-include_directories(SYSTEM ${GB_DEFAULT_INSTALL_DIR})
+include_directories(SYSTEM ${FS_DEFAULT_INSTALL_DIR})
 # search class_name.[h|cpp] in class_dir and add to buffer
-macro(gb_add_class class_name class_dir buffer)
+macro(fs_add_class class_name class_dir buffer)
   
   set(src_file ${CMAKE_CURRENT_SOURCE_DIR}/${class_dir}/${class_name}.cpp)
   if(EXISTS ${src_file})
