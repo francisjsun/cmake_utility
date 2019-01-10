04a752d69f35af2707b0e0c22eb4d7cec225c9b1
Francis Sun <francisjsun@outlook.com>
2019-01-10 18:04:47 +0800

    added #features: fs_proj, fs_pkg added

---
+++ pkg.sh
@@ -0,0 +1,12 @@
+# Copyright (c) 2019, F.S.. All rights reserved.
+#!/bin/bash
+pkg_name=fsCMake
+pkg_files="CMakeLists.txt src_pkg.sh.in"
+
+echo "******** packing fsCMake... ********"
+
+pkg_file_name=${pkg_name}.tar.xz
+tar -cvJf ${pkg_name}.tar.xz $pkg_files
+
+echo "******** $pkg_file_name generated ********"
+echo "******** end of packing fsCMake ********"
