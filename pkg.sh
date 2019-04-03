6f26ce238163967aad56a3c5d9623073a0a489a1
Francis Sun <francisjsun@outlook.com>
2019-04-03 12:46:35 +0800

    add build.sh in pkg

--- pkg.sh
+++ pkg.sh
@@ -1,9 +1,9 @@
 # Copyright (c) 2019, F.S.. All rights reserved.
 #!/bin/bash
 pkg_name=fsCMake
-pkg_files="CMakeLists.txt src_pkg.sh.in fs_proj.xml.in"
+pkg_files="CMakeLists.txt src_pkg.sh.in fs_proj.xml.in build.sh"
 
-echo "******** packing fsCMake... ********"
+echo "******** begin packing fsCMake ********"
 
 pkg_file_name=${pkg_name}.tar.xz
 tar -cvJf ${pkg_name}.tar.xz $pkg_files
