626c41c0c34ba039a421f0422d323eae695c7f5a
Francis Sun <francisjsun@outlook.com>
2019-06-20 15:37:44 +0800

    rename proj

--- pkg.py
+++ pkg.py
@@ -7,14 +7,14 @@ import shutil
 if __name__ == "__main__":
     pkg_files = ['CMakeLists.txt', 'src_pkg.py.in', 'fs_proj.xml.in',
                  'build.py', 'build.sh']
-    pkg_name = 'fsCMake'
+    pkg_name = 'cmake_utility'
     if os.path.isdir(pkg_name):
         shutil.rmtree(pkg_name)
     os.mkdir(pkg_name)
     for pkg_file in pkg_files:
         shutil.copy(pkg_file, pkg_name)
         
-    pkg_path = 'fsCMake.tar.xz'
+    pkg_path = 'cmake_utility.tar.xz'
     if os.path.isfile(pkg_path):
         os.remove(pkg_path)
     with tarfile.open(pkg_path, 'x:xz') as f:
