392731eb6e2473bd7e18bd79ed1ceb924f43ea6b
Francis Sun <francisjsun@outlook.com>
2020-01-07 20:57:55 +0800

    Change name to vesta

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# cmake_utility
-some common useful CMake scripts
+# vesta
+Some useful scripts for c++ project, including
+1. *CMakeLists.txt* A useful CMake script for c++ project.
+2. *config.h.in.in* Templte for config.h.in file.
+3. *vesta_pkg.py.in* Boilerplate for vesta_pkg.py, a python script for packing.
--- pkg.py
+++ pkg.py
@@ -1,22 +1,32 @@
-""" Copyright (c) 2019, F.S.. All rights reserved. """
+# Copyright (C) 2020 Francis Sun, all rights reserved.
+
 import os
 import tarfile
 import shutil
 
 
 if __name__ == "__main__":
-    pkg_files = ['CMakeLists.txt', 'fs_pkg.py.in', 'fs_proj.xml.in',
-                 'build.py']
-    pkg_name = 'cmake_utility'
+    pkg_files = ['CMakeLists.txt', 'config.h.in.in', 'vesta_pkg.py.in']
+    pkg_name = 'vesta'
+
+    # rm the dir pkg_name if it exists
     if os.path.isdir(pkg_name):
         shutil.rmtree(pkg_name)
+
+    # create the dir pkg_name
     os.mkdir(pkg_name)
+
+    #copy pkg_files into the dir pkg_name
     for pkg_file in pkg_files:
         shutil.copy(pkg_file, pkg_name)
         
-    pkg_path = 'cmake_utility.tar.xz'
+    pkg_path = 'vesta.tar.xz'
+
+    # rm the file if it exists
     if os.path.isfile(pkg_path):
         os.remove(pkg_path)
+
+    # tar it
     with tarfile.open(pkg_path, 'x:xz') as f:
         f.add(pkg_name)
         f.close()
