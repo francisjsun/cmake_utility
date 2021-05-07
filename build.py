5c59d647ad944b5397a9a43e1106706d56394d3a
Francis Sun <francisjsun@outlook.com>
2021-05-07 18:19:07 +0800

    ...

--- build.py
+++ build.py
@@ -14,13 +14,14 @@ def _clean():
 
 def _build(build_type="Debug"):
     if sys.platform == "win32":
-        subprocess.run(["cmake", ".."])
-        subprocess.run(["cmake", "--build", ".", "--config", build_type])
+        subprocess.run(["cmake", "-S", ".", "-B", "build"])
+        subprocess.run(["cmake", "--build", "build", "--config", build_type])
     else:
         cmake_build_type_arg = str.format("-DCMAKE_BUILD_TYPE={0}",
                                           build_type)
-        subprocess.run(["cmake", cmake_build_type_arg, ".."])
-        subprocess.run(["cmake", "--build", "."])
+        subprocess.run(["cmake", cmake_build_type_arg,
+                        "-S", ".", "-B", "build"])
+        subprocess.run(["cmake", "--build", "build"])
 
 
 def main():
@@ -34,18 +35,11 @@ def main():
     if opt.c:
         _clean()
     else:
-        if not os.path.isdir("build"):
-            os.mkdir("build")
-        old_cwd = os.getcwd()
-        os.chdir("build")
-
         if not opt.p:
             _build()
-            os.chdir(old_cwd)
         else:
             _build("Debug")
             _build("Release")
-            os.chdir(old_cwd)
             import vesta_pkg
             vesta_pkg.pkg()
 
